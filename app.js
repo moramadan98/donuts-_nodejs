e8ba3362dfed04de2a6745b5bf5c779ffc205792
Mahmoud M Abdelwahab <47304558+mahmoudmohamed22@users.noreply.github.com>
2022-03-19 23:47:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,3 @@ if(!donut) return res.status(404).send("NOT FOUND");
 res.send(donut);
 });
 
-
-app.put('/donuts/:id',(req,res)=>{
-    const donut = donuts.find(d=> d.id === parseInt(req.params.id));
-    if(!donut) return res.status(404).send("not found");
-    donut.type = req.body.name;
-    donut.cost = req.body.cost;
-    res.send(donut);
-});
-
-
