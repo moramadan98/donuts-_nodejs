50178c5b644b630b935b3243b63d2af55abbc91e
marwa adel <arwaadel00110@gmail.com>
2022-04-02 01:03:58 +0200

    edit get all donuts code

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 const express = require('express');
 const app = express();
 app.use(express.json());
+
 
 
 var donuts = [
@@ -18,10 +20,35 @@ app.put('/donuts/:id',(req,res)=>{
     res.send(donut);
 });
 
-
 app.get('/donuts',(req,res)=>{
   res.send(donuts);
 })
+
+app.post('/donuts',(req,res)=>{
+  const donut={
+    id:donuts.length+1,
+    type:req.body.type,
+    cost:req.body.cost
+  };
+  donuts.push(donut);
+  res.send(donut);
+});
+
+
+app.delete('/donuts/:id', (req, res) => {
+  const donut = donuts.find(d=> d.id === parseInt(req.params.id));
+  if(!donut) return res.status(404).send("NOT FOUND");
+  res.send(donut);
+});
+
+app.put('/donuts/:id',(req,res)=>{
+    const donut = donuts.find(d=> d.id === parseInt(req.params.id));
+    if(!donut) return res.status(404).send("not found");
+    donut.type = req.body.name;
+    donut.cost = req.body.cost;
+    res.send(donut);
+});
+
 
 const port = process.env.PORT || 3000 ;
 app.listen(port , ()=> {console.log(`Listen to port ${port}......`)});
